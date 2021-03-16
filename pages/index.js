bb61c2400a7e150e7c8f0c01ea618c4fe53c9a45
liangdong.xu <liangdong.xu@leyantech.com>
2021-03-16 10:36:33 +0800

    update .gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
 import React from "react";
+import MyLayout from "./components/MyLayout";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+// import "../styles.less"
 
-const Index = () => {
+
+const Index = ({ shows }) => {
     return (
-        <div>index</div>
+        <MyLayout>
+            <p>Home</p>
+            <ul>
+                {
+                    shows.map(item => (
+                        <li key={item.id}>
+                            <Link href={'/detail/[id]'} as={`/detail/${item.id}`}>
+                                <a>{item.name}</a>
+                            </Link>
+                            {/*<Link href={`/detail?id=${item.id}`}>*/}
+                            {/*    <a>{item.name}</a>*/}
+                            {/*</Link>*/}
+                        </li>
+                    ))
+                }
+            </ul>
+        </MyLayout>
     )
+}
+
+Index.getInitialProps = async () => {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const shows = await res.json()
+    return {
+        shows: shows.map(item => item.show)
+    }
 }
 
 export default Index
