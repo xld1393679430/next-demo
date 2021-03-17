9d6b8cf55936692070381aebf6cab9a12eb73550
liangdong.xu <liangdong.xu@leyantech.com>
2021-03-17 11:22:03 +0800

    修改：github oauth //todo

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import MyLayout from "../components/MyLayout";
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
+import { Button } from "antd";
 
 const Page = ({ shows = [] }) => {
+    // todo
+    const login = () => {
+        const CLIENT_ID = "Iv1.bc38f44ba1e26019";
+        const REDIRECT_URL = 'http://localhost:3002/about';
+        const url = "https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID + `&client_secret=${REDIRECT_URL}`;
+        window.location.href = url;
+    }
     return (
         <MyLayout>
+            <Button type="primary" icon="github" size="large" onClick={login}>
+                Github登录
+            </Button>
+
             <p>Home</p>
             <ul>
                 {
